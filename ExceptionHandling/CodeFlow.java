b073820e274085daf3f3bd836c23d3eefcf39c6a
igalna <igalna@users.noreply.github.com>
2015-05-14 17:37:22 +0100

    Create CodeFlow.java

---
+++ ExceptionHandling/CodeFlow.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class CodeFlow {
+
+    public static void main(String[] args) {
+        CodeFlow test = new CodeFlow();
+    
+        /* test.launch(0); */
+        /* test.launch(2); */
+        /* test.launch(4); */
+        test.launch(6);
+    }
+
+    public void launch(int userInput) {
+        
+        List<Integer> intList = new ArrayList<Integer>();
+        
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+        intList.add(5);
+        intList.add(6);
+        
+        try {
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+        } catch (IndexOutOfBoundsException ex) {
+            ex.printStackTrace();
+        }
+    }
