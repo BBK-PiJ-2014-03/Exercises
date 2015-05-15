b13f047cd962376f89ad69616a9b2fff2f8728c4
igalna <igalna@users.noreply.github.com>
2015-05-15 11:39:55 +0100

    Create TreeIntSet.java

---
+++ Trees/TreeIntSet.java
@@ -0,0 +1,15 @@
+
+public class TreeIntSet implements IntSet {
+    
+    private int value;
+    
+    private TreeIntSet left;
+    private TreeIntSet right;
+    
+    public TreeIntSet (int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+    
+    
